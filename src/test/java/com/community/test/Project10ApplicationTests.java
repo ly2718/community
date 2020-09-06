34d90cb4212602018961286693c8f76bca437d4a
LI YUAN <liyuan314@outlook.com>
2020-09-06 19:48:03 +0800

    init project

---
+++ src/test/java/com/community/test/Project10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
